a1297c534d6fac76e6620a6916b554633fd06730
PuppyPi <puppyofcodez@gmail.com>
2019-03-05 21:35:06 +0000

    Splitting rows now, but still not hairs :D

---
+++ Puppeteer/src/puppeteer/ATTParser.java
@@ -0,0 +1,11 @@
+package puppeteer;
+
+import puppeteer.JavaSpecificBits.WhatToDoWithEmpties;
+
+public class ATTParser
+{
+	public static String[] splitATTRow(String line)
+	{
+		return JavaSpecificBits.split(line, ' ', -1, WhatToDoWithEmpties.LeaveInEmpties);  //Change to LeaveOutEmpties to allow multiple spaces to count as a single space :3
+	}
+}
