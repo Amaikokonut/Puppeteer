265606f9d5249a6ff9b1d86f12b7d277162d8a43
Aiko <themaneda+github@gmail.com>
2019-03-16 21:23:30 -0400

    tests kinda work now

--- Puppeteer/src/puppeteer/testTester.java
+++ Puppeteer/src/puppeteer/testTester.java
@@ -9,15 +9,28 @@ public class testTester
 	public static void main() throws IOException
 	{
 		System.out.println("hi");
-//assumes you already generated a limbtests.txt file with testGenerator
+//THIS ASSUMES you already:
+		//generated a limbtests.txt file with testGenerator
+		//dropped that file into your Docking Station/Journal folder
+		//run tests.cos
+		//found "limbtestresults.txt" in your journal folder
+		//dropped limbtestresults.txt back into your working directory
+		//otherwise you're gonna get SO MANY ERRORS
+		
 		File file = new File("limbtests.txt");
 		String tests = (JavaSpecificBits.readAllTextUTF8(file));
 		String[] testLines = JavaSpecificBits.splitlines(tests);
 		
+		File enginefile = new File("limbtestresults.txt");
+		String enginetests = (JavaSpecificBits.readAllTextUTF8(enginefile));
+		String[] enginetestLines = JavaSpecificBits.splitlines(enginetests);
+		int index = 0;
+		int winning = 0;
+		
 		for (String x : testLines) {
 			//these are not ATTs but I guess we're testing the ATT Parser Today
 			String[] elements = ATTParser.splitATTRow(x);
-			//System.out.println(x);
+			System.out.println(x);
 			int part = Integer.valueOf(elements[0]);
 			int gspcs = CreatureInfo.GetSpcs(Integer.valueOf(elements[2]), Integer.valueOf(elements[1]));
 			int stage = Integer.valueOf(elements[3]);
@@ -29,16 +42,23 @@ public class testTester
 			File result = FileInfo.getClosestFile(part, gspcs, stage, slot, true);
 			if (result != null) {
 				//System.out.println(result.getName());
-				System.out.println("Got:    " + CreatureInfo.interpretedFilename(result.getName()));
+				System.out.println("You Got:" + CreatureInfo.interpretedFilename(result.getName()));
+				System.out.println((result.getName().substring(0,4)));
+				System.out.println("Game Got:" + CreatureInfo.interpretedFilename(enginetestLines[index]));
+				System.out.println(enginetestLines[index]);
+				
+				if (result.getName().substring(0,4).equals(enginetestLines[index])) {
+					winning++;
+				}
 				
 			} else {
 				System.out.println("Got: Null (this shouldn't happen :C)");
 			}
-			System.out.println(FileInfo.getClosestFile(part, gspcs, stage, slot, true).getName());
-			
+			//System.out.println(FileInfo.getClosestFile(part, gspcs, stage, slot, true).getName());
+			index++;
 			
 		}
-		
+		System.out.println("Game-True Matches: " + winning + " percent");
 		System.out.println("bye");
 		
 	}
