7e52a82d0df4b924d534fe9c716b95bfdff5bb42
PuppyPi <puppyofcodez@gmail.com>
2019-03-18 05:39:39 +0000

    Without this, it never diessssssss!
    Immortal Norn Programsssssss!  Nuuuuuuuuuuu!

--- Puppeteer/src/tests/testimage.java
+++ Puppeteer/src/tests/testimage.java
@@ -8,6 +8,7 @@ public class testimage
 	public static void main(String args[]) throws Exception
 	{
 		JFrame frame = new JFrame("Display image");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Panel panel = new ShowImage("backnorn.JPG");
 		frame.getContentPane().add(panel);
 		frame.setSize(300, 300);
